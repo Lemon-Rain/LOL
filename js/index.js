edd5b2f09401972169b364ca07b636419d8300b9
Lemon-Rain <2242195359@qq.com>
2020-06-03 10:44:30 +0800

    header模块开发V2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-var app = new VTTCue({
-    el: ".head_nav",
-    mothod: {
-        
+window.onscroll = function () {
+    if (window.scrollY > 50) {
+        document.querySelector("header").className = "small pr";
     }
-});
+}
+
+function subNavDown() {
+    var subNav = document.querySelector(".head_sub_nav");
+    subNav.className = "head_sub_nav pa show";
+}
+
+function subNavUp() {
+    var subNav = document.querySelector(".head_sub_nav");
+    subNav.className = "head_sub_nav pa hide";
+}
