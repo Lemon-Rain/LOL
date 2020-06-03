463d57af63e5422979fb5e11539bf91408d0cabc
Lemon-Rain <2242195359@qq.com>
2020-06-03 13:17:55 +0800

    header模块开发V3

--- js/index.js
+++ js/index.js
@@ -13,3 +13,13 @@ function subNavUp() {
     var subNav = document.querySelector(".head_sub_nav");
     subNav.className = "head_sub_nav pa hide";
 }
+
+function searchClose() {
+    var search = document.querySelector(".hover_search");
+    search.style.display = "none";
+}
+
+function searchOpen() {
+    var search = document.querySelector(".hover_search");
+    search.style.display = "block";
+}
