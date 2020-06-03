fba5bb9035c55215ae8d8d6fac27d04a78bfc617
Lemon-Rain <2242195359@qq.com>
2020-06-03 17:57:48 +0800

    header模块开发V5

--- js/index.js
+++ js/index.js
@@ -27,8 +27,8 @@ function searchOpen() {
 function display(name, model) {
     var el = document.querySelector(name);
     if (model == 'show') {
-        el.style.display = block;
+        el.style.display = "block";
     } else {
-        el.style.display = none;
+        el.style.display = "none";
     }
 }
